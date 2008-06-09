c978043008756ede271024da3c108bcc40eb219d
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2008-06-09 11:08:47 +0000

    Added generation of API documention, using JsDoc Toolkit, to the build script
    git-svn-id: http://js-forms.googlecode.com/svn/trunk@16 5718da49-034f-0410-9eff-a77264741840

--- build/build.py
+++ build/build.py
@@ -1,6 +1,9 @@
+import optparse
+import os
+
 from jsmin import jsmin
 
-FORMS_FILES = ['../util.js', '../widgets.js', '../fields.js', '../forms.js']
+SOURCE_FILES = ('../util.js', '../widgets.js', '../fields.js', '../forms.js')
 MODULE_DEFINITION = 'module.js'
 CODE_TEMPLATE = """var forms = function()
 {
@@ -9,10 +12,32 @@ CODE_TEMPLATE = """var forms = function()
 %(modules)s
 }();"""
 
-js = CODE_TEMPLATE % {
-    'code': '\n'.join([open(f, 'r').read() for f in FORMS_FILES]),
-    'modules': open(MODULE_DEFINITION).read(),
-}
+def main(generate_api=False, jsdoc_dir=None):
+    js = CODE_TEMPLATE % {
+        'code': '\n'.join([open(os.path.normpath(f), 'r').read()
+                           for f in SOURCE_FILES]),
+        'modules': open(MODULE_DEFINITION).read(),
+    }
 
-open('js-forms.js', 'w').write(js)
-open('js-forms-min.js', 'w').write(jsmin(js))
+    open('js-forms.js', 'w').write(js)
+    open('js-forms-min.js', 'w').write(jsmin(js))
+
+    if generate_api and jsdoc_dir:
+        os.system('java -jar %(jsdoc_jar)s %(jsdoc_app)s -a -t=%(jsdoc_templates)s -d=%(api_dir)s %(js_dir)s' % {
+            'jsdoc_jar': os.path.normpath(os.path.join(jsdoc_dir, 'jsrun.jar')),
+            'jsdoc_app': os.path.normpath(os.path.join(jsdoc_dir, 'app/run.js')),
+            'jsdoc_templates': os.path.normpath(os.path.join(jsdoc_dir, 'templates/jsdoc')),
+            'api_dir': os.path.normpath(os.path.join(os.path.dirname(__file__), 'api')),
+            'js_dir': os.path.normpath(os.path.join(os.path.dirname(__file__), '..')),
+        })
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser();
+    parser.add_option('-a', '--api', action='store_true', dest='generate_api',
+                      default=False,
+                      help="generate API documentation (requires JsDoc Toolkit "
+                            "- http://code.google.com/p/jsdoc-toolkit/)")
+    parser.add_option('-j', '--jsdoc', dest='jsdoc_dir', default=None,
+                      help="JsDoc Toolkit directory")
+    (options, args) = parser.parse_args()
+    main(options.generate_api, options.jsdoc_dir)
