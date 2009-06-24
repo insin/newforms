654b59b7c427d8aa2e55c2ea6947dc596bb1e046
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2009-06-24 10:59:20 +0000

    Added tests for ValidationError
    git-svn-id: http://js-forms.googlecode.com/svn/trunk@102 5718da49-034f-0410-9eff-a77264741840

--- tests/util.js
+++ tests/util.js
@@ -10,3 +10,33 @@ test("contains", function()
     ok(contains({"one": 1, "two": 2, "three": 3}, "two"), "Property in Object");
     ok(!contains({"one": 1, "two": 2, "three": 3}, "four"), "Property not in Object");
 });
+
+test("ValidationError", function()
+{
+    expect(5);
+    // Can take a string
+    equals(""+new ValidationError("There was an error.").messages.asUL(),
+           "<ul class=\"errorlist\"><li>There was an error.</li></ul>");
+
+    // Can take a list
+    equals(""+new ValidationError(["Error one.", "Error two."]).messages.asUL(),
+           "<ul class=\"errorlist\"><li>Error one.</li><li>Error two.</li></ul>");
+
+    // Can take a non-string
+    function VeryBadError()
+    {
+    }
+    VeryBadError.prototype =
+    {
+        toString: function() { return "A very bad error."; }
+    };
+    equals(""+new ValidationError(new VeryBadError()).messages.asUL(),
+           "<ul class=\"errorlist\"><li>A very bad error.</li></ul>");
+
+    // Escapes appropriately
+    var example = "Example of link: <a href=\"http://www.example.com/\">example</a>";
+    equals(""+new ValidationError(example).messages.asUL(),
+           "<ul class=\"errorlist\"><li>Example of link: &lt;a href=&quot;http://www.example.com/&quot;&gt;example&lt;/a&gt;</li></ul>");
+    equals(""+new ValidationError(DOMBuilder.markSafe(example)).messages.asUL(),
+           "<ul class=\"errorlist\"><li>Example of link: <a href=\"http://www.example.com/\">example</a></li></ul>");
+});
