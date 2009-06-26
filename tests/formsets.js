e7bbc8f7ed12d495c894ad11496cd574b7485f4d
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2009-06-26 15:57:48 +0000

    Formset testing WIP
    git-svn-id: http://js-forms.googlecode.com/svn/trunk@131 5718da49-034f-0410-9eff-a77264741840

--- tests/formsets.js
+++ tests/formsets.js
@@ -2,7 +2,7 @@ module("formsets");
 
 test("Basic formset creation and usage", function()
 {
-    expect(1);
+    expect(7);
 
     // FormSet allows us to use multiple instance of the same form on 1 page.
     // For now, the best way to create a FormSet is by using the formsetFactory
@@ -24,4 +24,53 @@ test("Basic formset creation and usage", function()
 "<tr><td colspan=\"2\"><input type=\"hidden\" name=\"choices-TOTAL_FORMS\" value=\"1\"><input type=\"hidden\" name=\"choices-INITIAL_FORMS\" value=\"0\"></td></tr>\n" +
 "<tr><th>Choice:</th><td><input type=\"text\" name=\"choices-0-choice\"></td></tr>\n" +
 "<tr><th>Votes:</th><td><input type=\"text\" name=\"choices-0-votes\"></td></tr>");
+
+    // One thing to note is that there needs to be a special value in the data.
+    // This value tells the FormSet how many forms were displayed so it can tell
+    // how many forms it needs to clean and validate. You could use javascript
+    // to create new forms on the client side, but they won't get validated
+    // unless you increment the TOTAL_FORMS field appropriately.
+    var data = {
+        "choices-TOTAL_FORMS": "1", // The number of forms rendered
+        "choices-INITIAL_FORMS": "0", // The number of forms with initial data
+        "choices-0-choice": "Calexico",
+        "choices-0-votes": "100"
+    };
+
+    // We treat FormSet pretty much like we would treat a normal Form. FormSet
+    // has an isValid method, and a cleanedData or errors attribute depending on
+    // whether all the forms passed validation. However, unlike a Form instance,
+    // cleaned_data and errors will be a list of objects rather than just a
+    // single object.
+    formset = new ChoiceFormSet({data: data, autoId: false, prefix: "choices"});
+    same(formset.isValid(), true);
+    same(formset.forms[0].cleanedData, {choice: "Calexico", votes: 100});
+
+    // If a FormSet was not passed any data, its isValid method should return
+    // false.
+    formset = new ChoiceFormSet();
+    same(formset.isValid(), false);
+
+    // FormSet instances can also have an error attribute if validation failed for
+    // any of the forms.
+    var data = {
+        "choices-TOTAL_FORMS": "1",
+        "choices-INITIAL_FORMS": "0",
+        "choices-0-choice": "Calexico",
+        "choices-0-votes": ""
+    };
+    formset = new ChoiceFormSet({data: data, autoId: false, prefix: "choices"});
+    same(formset.isValid(), false);
+    same(formset.errors[0].votes.errors, ["This field is required."]);
+
+    // We can also prefill a FormSet with existing data by providing an "initial"
+    // argument to the constructor, which should be a lis of objects.By default,
+    // an extra blank form is included.
+    var initial = [{choice: "Calexico", votes: 100}];
+    formset = new ChoiceFormSet({initial: initial, autoId: false, prefix: "choices"});
+    equals([formset.forms[0].asUL(), formset.forms[1].asUL()].join("\n"),
+"<li>Choice: <input type=\"text\" name=\"choices-0-choice\" value=\"Calexico\"></li>\n" +
+"<li>Votes: <input type=\"text\" name=\"choices-0-votes\" value=\"100\"></li>\n" +
+"<li>Choice: <input type=\"text\" name=\"choices-1-choice\"></li>\n" +
+"<li>Votes: <input type=\"text\" name=\"choices-1-votes\"></li>");
 });
