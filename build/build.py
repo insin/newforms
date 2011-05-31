99d6d2cae8a3d7f1c6b3762634f048a4877d4795
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2011-05-31 23:43:11 +0100

    Bumped version number

--- build/build.py
+++ build/build.py
@@ -1,6 +1,6 @@
 import os
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 TIME_SOURCE_FILES = ('../src/time.js',)
 FORMS_SOURCE_FILES = ('../src/util.js', '../src/validators.js',
                       '../src/widgets.js', '../src/fields.js',
