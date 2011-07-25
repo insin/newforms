e8b237d113594d8bd05a2b2b58cc774bc9518643
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2011-07-25 23:04:00 +0100

    Fixed busted package.json; added local node_modules to test paths

--- tests/tests.js
+++ tests/tests.js
@@ -1,17 +1,20 @@
 var qunit = require('qunit'),
     path = require('path');
 
-qunit.options.coverage = false;
+// Built newforms library
+var lib = '../newforms.js';
+
+// Add local node_modules to require.paths() in test processes, so DOMBuilder
+// doesn't need to be installed globally.
+qunit.options.paths = '../node_modules'
 qunit.options.deps = [
   {path: './customAsserts.js'},
   {path: 'DOMBuilder', namespace: 'DOMBuilder'}
 ];
 
-var lib = '../newforms.js';
-
 path.exists(lib, function(exists) {
   if (!exists) {
-    console.log(lib + " doesn't exist - run build.py to create it");
+    console.error(lib + " doesn't exist - run build.py to create it");
     return;
   }
 
