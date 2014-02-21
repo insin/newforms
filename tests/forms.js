1bc1f5f5f0e9b23ee22d2300ff170846d96f94c0
insin <jonathan.buchanan@gmail.com>
2014-02-21 11:46:07 +0000

    Added tests for BaseForm#addError
    Added ErrorObject#hasField to check for existing field error messages

--- tests/forms.js
+++ tests/forms.js
@@ -707,7 +707,7 @@ QUnit.test("Escaping", 2, function() {
 })
 */
 
-QUnit.test("Validating multiple fields", 20, function() {
+QUnit.test("Validating multiple fields", 26, function() {
   // There are a couple of ways to do multiple-field validation. If you want
   // the validation message to be associated with a particular field,
   // implement the clean_XXX() method on the Form, where XXX is the field
@@ -743,11 +743,15 @@ QUnit.test("Validating multiple fields", 20, function() {
   equal(f.cleanedData.password2, "foo")
 
   // Another way of doing multiple-field validation is by implementing the
-  // Form's clean() method. If you do this, any ValidationError raised by that
-  // method will not be associated with a particular field; it will have a
-  // special-case association with the field named '__all__'.
-  // Note that in Form.clean(), you have access to self.cleanedData, an object
-  // containing all the fields/values that have *not* raised a
+  // Form's clean() method. Usually a ValidationError raised by that method
+  // will not be associated with a particular field and will have a
+  // special-case association with the field named '__all__'. It's
+  // possible to associate the errors to particular field with the
+  // Form.addError() method or by passing a on object that maps each field to
+  // one or more errors.
+  //
+  // Note that in Form.clean(), you have access to this.cleanedData, an
+  // object containing all the fields/values that have *not* raised a
   // ValidationError. Also note Form.clean() is required to return a
   // dictionary of all clean data.
   UserRegistration = forms.Form.extend({
@@ -756,14 +760,33 @@ QUnit.test("Validating multiple fields", 20, function() {
   , password2: forms.CharField({widget: forms.PasswordInput})
 
   , clean: function() {
+      // Test raising a ValidationError as NON_FIELD_ERRORS
       if (this.cleanedData.password1 && this.cleanedData.password2 &&
           this.cleanedData.password1 != this.cleanedData.password2) {
         throw forms.ValidationError("Please make sure your passwords match.")
       }
 
-      // TODO Test raising ValidationError that targets multiple fields.
+      // Test raising ValidationError that targets multiple fields
+      var errors = {}
+      if (this.cleanedData.password1 == 'FORBIDDEN_VALUE') {
+        errors.password1 = 'Forbidden value.'
+      }
+      if (this.cleanedData.password2 == 'FORBIDDEN_VALUE') {
+        errors.password2 = 'Forbidden value.'
+      }
+      if (Object.keys(errors).length) {
+        throw forms.ValidationError(errors)
+      }
 
-      // TODO Implememt and test Form.addError()
+      // Test Form.addError()
+      if (this.cleanedData.password1 == 'FORBIDDEN_VALUE2') {
+        this.addError(null, 'Non-field error 1.')
+        this.addError('password1', 'Forbidden value 2.')
+      }
+      if (this.cleanedData.password2 == 'FORBIDDEN_VALUE2') {
+        this.addError('password2', 'Forbidden value 2.')
+        throw forms.ValidationError('Non-field error 2.')
+      }
 
       return this.cleanedData
     }
@@ -796,6 +819,17 @@ QUnit.test("Validating multiple fields", 20, function() {
   equal(f.cleanedData.username, "adrian")
   equal(f.cleanedData.password1, "foo")
   equal(f.cleanedData.password2, "foo")
+
+  f = new UserRegistration({data: {username: 'adrian', password1: 'FORBIDDEN_VALUE', password2: 'FORBIDDEN_VALUE'}, autoId: false})
+  deepEqual(f.errors('password1').messages(), ['Forbidden value.'])
+  deepEqual(f.errors('password2').messages(), ['Forbidden value.'])
+
+  f = new UserRegistration({data: {username: 'adrian', password1: 'FORBIDDEN_VALUE2', password2: 'FORBIDDEN_VALUE2'}, autoId: false})
+  deepEqual(f.errors('__all__').messages(), ['Non-field error 1.', 'Non-field error 2.'])
+  deepEqual(f.errors('password1').messages(), ['Forbidden value 2.'])
+  deepEqual(f.errors('password2').messages(), ['Forbidden value 2.'])
+
+  throws(f.addError.bind(f, 'missingField', 'Some error.'))
 })
 
 QUnit.test("Dynamic construction", 17, function() {
