dee57a9375fce00763ae36e6b6f93cdb7841b9f8
insin <jonathan.buchanan@gmail.com>
2014-11-22 21:10:00 +0000

    [docs] Added a specfying-clean-fields example
    [ci skip]

---
+++ docs/src/examples/specifying-clean-fields.jsx
@@ -0,0 +1,21 @@
+'use strict';
+
+var React = require('react')
+var forms = require('newforms')
+
+var FormRenderer = require('../FormRenderer')
+
+var PersonForm = forms.Form.extend({
+  firstName: forms.CharField({required: false, maxLength: 50}),
+  lastName: forms.CharField({required: false, maxLength: 50}),
+  jobTitle: forms.CharField({required: false, maxLength: 100}),
+  organisation : forms.CharField({required: false}),
+
+  clean: ['firstName', 'lastName', function() {
+     if (!this.cleanedData.firstName && !this.cleanedData.lastName) {
+       throw forms.ValidationError('A first name or last name is required.')
+     }
+  }]
+})
+
+React.render(<FormRenderer form={PersonForm}/>, document.body)
