50fe3f1dea1c6e68f18216d5bbea435cbed20e28
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2014-11-25 09:58:34 +0000

    [fixed] The argument to Form.extend() is no longer mutated
    Quick patch to ahallow-copy prototypeProps, this will need to be fixed in Concur

--- tests/regressions.js
+++ tests/regressions.js
@@ -15,3 +15,20 @@ QUnit.test("deepCopy", 1, function() {
     strictEqual(f.isValid(), true,
                 "Copied RegExp fields don't throw exceptions when validating")
 })
+
+QUnit.test("#54 - Form.extend() mutates its argument", 1, function() {
+  var field1 = forms.CharField()
+  var field2 = forms.CharField()
+  var fields = {
+    type1: {
+      field: field1
+    },
+    type2: {
+      field: field2
+    }
+  }
+
+  var Type1Form = forms.Form.extend(fields.type1)
+  var Type2Form = forms.Form.extend(fields.type2)
+  deepEqual(fields, {type1: {field: field1}, type2: {field: field2}})
+})
