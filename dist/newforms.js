d614a2730a484c508415b741b95d18b810d86184
Adam Becker <ad@mbecker.cc>
2017-08-30 13:05:43 -0700

    dist

--- dist/newforms.js
+++ dist/newforms.js
@@ -1,5 +1,5 @@
 /**
- * newforms 0.13.2 (dev build at Wed, 30 Aug 2017 19:44:14 GMT) - https://github.com/insin/newforms
+ * newforms 0.13.2 (dev build at Wed, 30 Aug 2017 20:05:30 GMT) - https://github.com/insin/newforms
  * MIT Licensed
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.forms = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -13066,6 +13066,7 @@ var ReactElementValidator = {
 };
 
 module.exports = ReactElementValidator;
+
 },{"./ReactComponentTreeHook":106,"./ReactCurrentOwner":107,"./ReactElement":109,"./canDefineProperty":117,"./checkReactTypeSpec":118,"./getIteratorFn":120,"./lowPriorityWarning":121,"fbjs/lib/warning":86}],112:[function(require,module,exports){
 /**
  * Copyright 2015-present, Facebook, Inc.
